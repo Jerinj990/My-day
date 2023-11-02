05f3032ef8961d4cb26b181ef7156a14cb326e66
Jerinj990 <123081108+Jerinj990@users.noreply.github.com>
2023-11-02 15:15:01 +0000

    Python files for the admin panel

---
+++ myday/main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from admin import admin
+from public import public
+from api import api
+
+
+app = Flask(__name__)
+app.secret_key='asdfg'
+app.register_blueprint(public)
+app.register_blueprint(admin,url_prefix="/admin")
+app.register_blueprint(api,url_prefix="/api")
+
+app.run(debug=True,port=5001,host="192.168.144.57")
